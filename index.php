6841199f572b76775c04ffd9fe91e4bc0f249f1d
MohamadOuf <72459027+MohamadOuf@users.noreply.github.com>
2020-11-19 09:51:25 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ if (isset($_GET{["FIrstName"])){//show feedback
 echo $_GET{["FIrstName"];
 }else{//show form
 echo'
-<form>
-first Name: <input type="text" name="FIrstName" /><br />
+<form> "action"
+first Name: <input type="text" name="FirstName" /><br />
 <input type="submit" />
 </form>
 
